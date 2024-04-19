117f321aee66262fb9da63f4aa9805dd0be9e64c
juguedev <juguer.anra@gmail.com>
2024-04-19 16:41:11 -0500

    Update lambda code

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -2,11 +2,37 @@ import json
 import logging
 import boto3
 import os
-
+from boto3.dynamodb.conditions import Key
+from statistics import mean, median, mode
+from datetime import datetime
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 TBL_NAME = os.environ['TBL_NAME']
 
+
+def calculate_aggregation(response, aggregation, feature_id, device_id):
+    values = [item[feature_id] for item in response['Items']]
+    timestamp = [item['timestamp'] for item in response['Items']]
+
+    if aggregation == 'mean':
+        return f"The mean value of the feature {feature_id} for the device {device_id} is {mean(values)}"
+    
+    elif aggregation == 'latest':
+        latest_timestamp = datetime.strptime(str(timestamp[0]), '%Y%m%d%H%M%S')
+        print(timestamp)
+        return f"The last value of the feature {feature_id} for the device {device_id} is {values[0]} at {str(latest_timestamp)}"
+        
+    elif aggregation == 'range':
+        return f"The range of values of the feature {feature_id} for the device {device_id} is from {min(values)} to {max(values)}"
+
+    elif aggregation == 'mode':
+        return f"The mode value of the feature {feature_id} for the device {device_id} is {mode(values)}"
+    
+    elif aggregation == 'median':
+        return f"The median value of the feature {feature_id} for the device {device_id} is {median(values)}"
+
+    else:
+        return None
 
 def lambda_handler(event, context):
     logger.info('event={}'.format(event))
@@ -23,7 +49,7 @@ def lambda_handler(event, context):
     logger.info(f"request received for aggregation: {aggregation}")
 
     
-    data = get_dynamo_data(device_name, feature_name)
+    data = get_dynamo_data(device_name, feature_name, aggregation)
     print(data)
     
     return close(data, 'Fulfilled', {'contentType': 'PlainText','content': "response-test"})  
@@ -53,7 +79,7 @@ def close(event_data, fulfillment_state, message):
     return response
 
 
-def get_dynamo_data(device_id, feature_id):
+def get_dynamo_data(device_id, feature_id, aggregation_id):
     """
     Query DynamoDB for data.
 
@@ -69,12 +95,19 @@ def get_dynamo_data(device_id, feature_id):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TBL_NAME)
 
-    response = table.get_item(
-        Key={
-            'device_id': device_id
-        }
+    #response = table.get_item(
+    #    Key={
+    #        'device_id': device_id
+    #    }
+    #)
+    response = table.query(
+        KeyConditionExpression=Key('device_id').eq(device_id),
+        ScanIndexForward=False,  # Reverse the order of results
+        Limit=600  # Limit the result to one item
     )
+    
+   
+    result_aggregated = calculate_aggregation(response, aggregation_id, feature_id , device_id)
+    print(result_aggregated)
 
-    print(str(response['Item'][feature_id]))
-
-    return f"The value of the feature {feature_id} for the device {device_id} is {str(response['Item'][feature_id])}"
+    return result_aggregated
